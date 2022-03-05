8ee552ac17067ce3c24e99eeb97f5e3acc427d69
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2022-03-05 15:29:26 -0300

    Ajustes no Layout

---
+++ app/Http/Controllers/FornecedorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FornecedorController extends Controller
+{
+    public function index(){
+
+        $fornecedores = ['Fornecedores 1'];
+
+        return view('app.fornecedores.index', compact('fornecedores'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Olá seja bem vindo ao curso';
+// Route::get('/', function () {
+//     return 'Olá seja bem vindo ao curso';
+// });
+
+Route::get('/', 'PrincipalController@principal')->name('site.principal');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
 });
 
-Route::get('/contato', function () {
-    return 'Página de Contato';
-});
-
-Route::get('/sobre-nos', function () {
-    return 'Página Sobre Nós';
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.principal').'">clique aqui</a> para ir para página inicial';
 });
 
 
+
+
+
+/* Verbos HTTP
+
+Route::get(uri,callback)
+
+get
+post
+put
+patch
+delete
+options
+
+*/
